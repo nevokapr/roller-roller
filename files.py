2d477eeee88acbaa85eabc61171af1aa9652e1ad
Nevokapr <nevokapr@yandex.ru>
2022-12-07 20:50:53 +0600

    finally fixed path strings (I hope)

--- files.py
+++ files.py
@@ -12,7 +12,7 @@ false_starts = [
     ]
 
 def err_log(error):
-    with open(f'{filespath}logs.txt', 'a+', encoding='UTF-8') as logs:
+    with open(f'{filespath}/logs.txt', 'a+', encoding='UTF-8') as logs:
         logs.write(f'\n\n{error}, {now()}')
     return "Произошла ошибка, свяжитесь с @Nevokapr"
 
@@ -20,7 +20,7 @@ def add_sugg(user_fullname, user_id, text):
     if text in false_starts:
         return 'Вы ничего не предложили.\nНапишите команду /suggest руками, и напишите текст после неё. **В одном сообщении.**'
     try:
-        with open(f"{filespath}suggestions.csv", "a+", encoding="utf-8") as file:
+        with open(f"{filespath}/suggestions.csv", "a+", encoding="utf-8") as file:
             file.write(f'\n\n{user_id}, {user_fullname}, {text}')
     except Exception as e:
         return err_log(e)
@@ -28,7 +28,7 @@ def add_sugg(user_fullname, user_id, text):
 
 def grab(user_fullname, user_name, user_id, func, text=''):
     try:
-        with open(f"{filespath}", "a+", encoding="utf-8") as file:
+        with open(f"{filespath}/users.csv", "a+", encoding="utf-8") as file:
             file.write(f'\n\n{user_fullname}, {user_name}, {user_id}, {func}, {text}, {now()}')
     except Exception as e:
         return err_log(e)
